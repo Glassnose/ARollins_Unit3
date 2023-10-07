9600536b5f8ce77b88afc88a75dda8010a6c54b3
Glassnose <rollinsasia@yahoo.com>
2023-10-07 16:24:34 -0700

    Tags

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed;
+    private PlayerController playerCtrl;
+    private float leftBounds = -9;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCtrl = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerCtrl.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBounds && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+        
+       
     }
 }
